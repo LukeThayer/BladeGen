f7bf05d9a7406f07a236cc64f61537911d0f5d31
Luke Thayer <lukemanthayer@gmail.com>
2020-01-29 11:04:28 -0800

    first commit

---
+++ src/main_menu.gd
@@ -0,0 +1,60 @@
+extends CanvasLayer
+
+func _ready():
+	network.connect("server_created", self, "_on_ready_to_play")
+	network.connect("join_success", self, "_on_ready_to_play")
+	network.connect("join_fail", self, "_on_join_fail")
+
+func _on_btCreate_pressed() -> void:
+	
+	# Properly set the local player information
+	set_player_info()
+	
+	# Gather values from the GUI and fill the network.server_info dictionary
+	if (!$PanelHost/txtServerName.text.empty()):
+		network.server_info.name = $PanelHost/txtServerName.text
+	network.server_info.max_players = int($PanelHost/txtMaxPlayers.value)
+	network.server_info.used_port = int($PanelHost/txtServerPort.text)
+	
+	# And create the server, using the function previously added into the code
+	network.create_server()
+	
+
+
+
+
+
+func set_player_info():
+	if (!$PanelPlayer/txtPlayerName.text.empty()):
+		gamestate.player_info.name = $PanelPlayer/txtPlayerName.text
+	gamestate.player_info.char_color = $PanelPlayer/btColor.color
+
+
+
+
+
+func _on_ready_to_play():
+	get_tree().change_scene("res://assets/levels/test_level.tscn")
+
+
+
+
+
+func _on_join_fail():
+	print("Failed to join server")
+
+func _on_btJoin_pressed() -> void:
+	
+	# Properly set the local player information
+	set_player_info()
+	var port = int($PanelJoin/txtJoinPort.text)
+	var ip = $PanelJoin/txtJoinIP.text
+	network.join_server(ip, port)
+	
+
+
+
+
+
+
+
